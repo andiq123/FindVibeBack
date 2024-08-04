9e9e37bb8cca209702f3f7c36e743f59ee2619ea
Andi <andy.u789@gmail.com>
2024-08-04 19:53:59 +0300

    sadas

--- API/Services/GenerateConnectionStrings.cs
+++ API/Services/GenerateConnectionStrings.cs
@@ -14,16 +14,16 @@ public static class GenerateConnectionStrings
 
         // Parse connection URL to connection string for Npgsql
         connUrl = connUrl.Replace("postgres://", string.Empty);
-        var pgUserPass = connUrl.Split("@")[0];
-        var pgHostPortDb = connUrl.Split("@")[1];
-        var pgHostPort = pgHostPortDb.Split("/")[0];
-        var pgDb = pgHostPortDb.Split("/")[1];
-        var pgUser = pgUserPass.Split(":")[0];
-        var pgPass = pgUserPass.Split(":")[1];
-        var pgHost = pgHostPort.Split(":")[0];
-        var pgPort = pgHostPort.Split(":")[1];
+        var pgUserPass = connUrl.Split('@')[0];
+        var pgHostPortDb = connUrl.Split('@')[1];
+        var pgHostPort = pgHostPortDb.Split('/')[0];
+        var pgDb = pgHostPortDb.Split('/')[1];
+        var pgUser = pgUserPass.Split(':')[0];
+        var pgPass = pgUserPass.Split(':')[1];
+        var pgHost = pgHostPort.Split(':')[0];
+        var pgPort = pgHostPort.Split(':')[1];
         
         return
-            $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}; SSL Mode=Require; Trust Server Certificate=true";
+            $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL Mode=Require;Trust Server Certificate=true";
     }
 }
