3b8a7f00a84e81d6d1a1e1a4c8227e344bc06990
Andi <andy.u789@gmail.com>
2024-08-04 18:40:43 +0300

    config

--- API/Services/GenerateConnectionStrings.cs
+++ API/Services/GenerateConnectionStrings.cs
@@ -2,13 +2,13 @@ namespace API.Services;
 
 public static class GenerateConnectionStrings
 {
-    public static string GetConnectionString(ConfigurationManager configurationManager)
+    public static string GetConnectionString(IConfiguration configuration)
     {
-        var host = configurationManager.GetSection("ConnectionStrings")["Hostname"];
-        var port = configurationManager.GetSection("ConnectionStrings")["Port"];
-        var database = configurationManager.GetSection("ConnectionStrings")["Database"];
-        var username = configurationManager.GetSection("ConnectionStrings")["Username"];
-        var password = configurationManager.GetSection("ConnectionStrings")["Password"];
+        var host = configuration["DBHost"];
+        var port = configuration["Port"];
+        var database = configuration["Database"];
+        var username = configuration["DbUser"];
+        var password = configuration["Password"];
         return $"Host={host};Port={port};Username={username};Password={password};Database={database};";
     }
 }
